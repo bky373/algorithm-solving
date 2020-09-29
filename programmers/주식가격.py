0163d1092dfb2be72edf73abafbbf972e43acdc2
bky373 <bo373@naver.com>
2020-09-29 18:41:18 +0900

    solve 주식가격 (python, 200920)

--- programmers/주식가격.py
+++ programmers/주식가격.py
@@ -1,3 +1,23 @@
+from collections import deque
+
+def solution(prices):
+    prices_dq = deque(prices)
+    answer = [0]*len(prices)
+
+    if len(prices_dq) == 2:
+        return [1,0]
+
+    for x in range(len(prices_dq)):
+        current_price = prices_dq.popleft()
+        
+        for next_price in prices_dq:
+            answer[x] += 1
+            if current_price > next_price:
+                break
+    return answer
+
+print(solution([1, 2, 3, 2, 3])) # [4, 3, 1, 1, 0]
+
 
 """
 <반복문 로직>
@@ -26,27 +46,4 @@ C를 D와 비교한다
 *마지막*
 D는 내부 반복문을 돌기 전에 빠져나온다
 
-"""
-from collections import deque
-
-def solution(prices):
-    prices_dq = deque(prices)
-    answer = [0]*len(prices)
-
-    if len(prices_dq) == 2:
-        return [1,0]
-
-    for x in range(len(prices_dq)):
-        current_price = prices_dq.popleft()
-        
-        for next_price in prices_dq:
-            answer[x] += 1
-            if current_price > next_price:
-                break
-    return answer
-
-print(solution([1, 2, 3, 2, 3]))
-
-"""
-[4, 3, 1, 1, 0]
 """
