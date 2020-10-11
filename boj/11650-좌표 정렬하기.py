f7f8dba9f863b1c3cef84d9f6b3e674b3603986d
bky373 <bo373@naver.com>
2020-10-11 11:31:35 +0900

    solve 11650-좌표 정렬하기 (python, 2010010)

---
+++ boj/11650-좌표 정렬하기.py
@@ -0,0 +1,10 @@
+n = int(input())
+positions = []
+for _ in range(n):
+    data = input().split()
+    positions.append((int(data[0]), int(data[1])))
+
+positions_sorted = sorted(positions)
+
+for p in positions_sorted:
+    print(p[0], p[1])
