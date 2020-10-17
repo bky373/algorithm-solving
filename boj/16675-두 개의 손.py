f982dc152b70f9833631e4c8b72ac689044b4f97
bky373 <bo373@naver.com>
2020-10-17 11:27:14 +0900

    solve 16675-두 개의 손 (python, 201017)

---
+++ boj/16675-두 개의 손.py
@@ -0,0 +1,11 @@
+"""
+    S, R, P: 가위, 바위, 보
+"""
+ML, MR, TL, TR = ('SRP'.index(i) for i in input().split())
+
+if ML == MR and (ML+1) % 3 in [TL, TR]:
+     print("TK") 
+elif TL == TR and (TL+1) % 3 in [ML, MR]:
+    print("MS")
+else:
+    print("?")
