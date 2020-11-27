36a7709d68226bf9be3f382e07a1a6e176fc22ba
bky373 <bo373@naver.com>
2020-11-27 23:22:20 +0900

    [20/11/27] solve 2442번-별 찍기-5

---
+++ by-kotlin/kt-boj/solutions/2442-별 찍기-5.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+    val blank = " "
+    val star = "*"
+
+    for (i in 1..n) {
+        for (j in 1..n - i) {
+            print(blank)
+        }
+        for (j in 1..i) {
+            print(star)
+        }
+        for (j in 1 until i) {
+            print(star)
+        }
+        println()
+    }
+}
