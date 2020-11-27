a360b3ffe4d8e22584435388b33e0740acb14626
bky373 <bo373@naver.com>
2020-11-27 19:28:43 +0900

    [20/11/27] solve 1100번-하얀 칸

---
+++ by-kotlin/kt-boj/solutions/1100-하얀 칸.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val board = Array(8) { Array(8) { "" } }
+    var result = 0
+    
+    (0 until 8).forEach { i ->
+        val row = readLine()
+        (0 until 8).forEach { j ->
+            board[i][j] = row[j].toString()
+            if ((i + j) % 2 == 0) {
+                if (board[i][j] == "F") {
+                    result += 1
+                }
+            }
+        }
+    }
+    println(result)
+}
