b83fdedb5070d03fd1d619eb0bb5f87abe45bea5
bky373 <bo373@naver.com>
2020-11-28 11:53:06 +0900

    [20/11/28] solve 2444번-별 찍기-7

---
+++ by-kotlin/kt-boj/solutions/2444-별 찍기-7.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+
+    fun blank(n: Int): String {
+        if (n == 0) {
+            return ""
+        }
+        if (n == 1) {
+            return " "
+        }
+        return " " + blank(n - 1)
+    }
+
+    fun star(n: Int): String {
+        if (n == 0) {
+            return ""
+        }
+        if (n == 1) {
+            return "*"
+        }
+        return "**" + star(n - 1)
+    }
+
+    (1 until n).forEach { i ->
+        println(blank(n - i) + star(i))
+    }
+    (0 until n).forEach { i ->
+        println(blank(i) + star(n - i))
+    }
+}
