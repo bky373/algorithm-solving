332e1434e9ac4ae06700372f2060543b78c93bdb
bky373 <bo373@naver.com>
2020-11-30 15:25:47 +0900

    [20/11/30] solve 2448번-별 찍기-10

---
+++ by-python/py-boj/2448-별 찍기-10.py
@@ -0,0 +1,26 @@
+import sys
+
+sys.setrecursionlimit(1000000000)
+
+n = int(input())
+arr = [[' '] * n for _ in range(n)]
+
+
+def solve(y, x, n):
+    if n == 1:
+        arr[y][x] = '*'
+        return
+
+    div = n // 3
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            else:
+                solve(y + (i * div), x + (j * div), div)
+
+
+solve(0, 0, n)
+
+for a in arr:
+    print(''.join([c for c in a]))
