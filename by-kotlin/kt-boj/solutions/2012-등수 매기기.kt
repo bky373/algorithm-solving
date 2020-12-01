72484372fd3e5ed73158ab314865345b577b3367
bky373 <bo373@naver.com>
2020-12-01 17:18:13 +0900

    [20/12/01] solve 2012번-등수 매기기

--- by-kotlin/kt-boj/solutions/2012-등수 매기기.kt
+++ by-kotlin/kt-boj/solutions/2012-등수 매기기.kt
@@ -1,6 +1,21 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.math.BigDecimal
+import kotlin.math.abs
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+    val data = mutableListOf<Int>()
+    var result = BigDecimal.ZERO
 
+    repeat(n) {
+        data.add(readLine().toInt())
+    }
+    data.sort()
+
+    (1..n).forEach { i ->
+        result += abs(i - data[i - 1]).toBigDecimal()
+    }
+
+    print(result)
 }
