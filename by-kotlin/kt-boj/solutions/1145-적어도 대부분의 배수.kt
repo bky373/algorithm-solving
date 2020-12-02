50c4a47863d84340ccc40c17aadfefe300d459e4
bky373 <bo373@naver.com>
2020-12-02 15:41:03 +0900

    [20/12/02] solve 1145번-적어도 대부분의 배수

---
+++ by-kotlin/kt-boj/solutions/1145-적어도 대부분의 배수.kt
@@ -0,0 +1,27 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val numbers = mutableListOf<Int>()
+    repeat(5) { numbers.add(nextInt()) }
+
+    numbers.sort()
+
+    var result = 0
+    val maximum = numbers[2] * numbers[3] * numbers[4]
+    
+    loop@ for (k in numbers[0]..maximum + 1) {
+        var count = 0
+        for (i in numbers.indices) {
+            if (k % numbers[i] == 0 && k >= numbers[i]) {
+                count += 1
+            }
+            if (count >= 3) {
+                result = k
+                break@loop
+            }
+        }
+    }
+
+    print(result)
+
+}
