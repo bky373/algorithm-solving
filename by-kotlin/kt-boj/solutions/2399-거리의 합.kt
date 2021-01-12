e4413ce1a101663d83ced2275a7a8edc5313a01f
bky373 <bo373@naver.com>
2021-01-12 15:26:04 +0900

    [21/01/12] solve 2399번-거리의 합 (change Int value to BigInteger)

--- by-kotlin/kt-boj/solutions/2399-거리의 합.kt
+++ by-kotlin/kt-boj/solutions/2399-거리의 합.kt
@@ -13,13 +13,14 @@
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.math.BigInteger
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val n = readLine().toInt()
-    val arr = readLine().split(" ").map { it.toInt() }.sorted()
+    val arr = readLine().split(" ").map { it.toBigInteger() }.sorted()
 
-    var result = 0
+    var result = BigInteger.ZERO
     for (i in arr.indices)
-        result += arr[i] * (2 * i - 2 * (n - i - 1))
+        result = result.plus(arr[i].multiply((2 * i - 2 * (n - i - 1)).toBigInteger()))
     println(result)
 }
