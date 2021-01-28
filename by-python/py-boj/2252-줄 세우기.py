6914545137f58138dafbd1fec4ba0ebf7b60ccdc
bky373 <bo373@naver.com>
2021-01-28 14:53:57 +0900

    [21/01/28] solve 2252 - 줄 세우기 including dfs solution

--- by-python/py-boj/2252-줄 세우기.py
+++ by-python/py-boj/2252-줄 세우기.py
@@ -1,5 +1,6 @@
 """
-학습 참고: https://m.blog.naver.com/ndb796/221236874984 (나동빈님 블로그)
+- 큐와 indegree의 수를 이용한 방법
+- 학습 참고(1): https://m.blog.naver.com/ndb796/221236874984 (나동빈님 블로그)
 """
 
 
@@ -26,7 +27,7 @@ def topological_sort(degrees):
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    adj = [[] * (n + 1) for _ in range(n + 1)]
+    adj = [[] for _ in range(n + 1)]
     degrees = [0] * (n + 1)
     res_line = [0] * n
 
@@ -36,3 +37,37 @@ if __name__ == '__main__':
         degrees[b] += 1
 
     topological_sort(degrees)
+
+
+
+"""
+- 스택과 DFS를 이용한 방법
+- 학습 참고(2): https://jason9319.tistory.com/93
+"""
+# def dfs(x):
+#     global st
+#     tour[x] = 1
+#
+#     for new in adj[x]:
+#         if tour[new]: continue
+#         dfs(new)
+#
+#     st.append(x)
+#
+#
+# if __name__ == '__main__':
+#     n, m = map(int, input().split())
+#     adj = [[] for _ in range(n + 1)]
+#     tour = [0] * (n + 1)
+#     st = []
+#
+#     for _ in range(m):
+#         a, b = map(int, input().split())
+#         adj[a].append(b)
+#
+#     for i in range(1, n + 1):
+#         if not tour[i]:
+#             dfs(i)
+#
+#     while st:
+#         print(st.pop(), end=' ')
