7f13b5adce86d95f98eb5f1c7236bda0de84e8ff
bky373 <bo373@naver.com>
2021-01-28 15:36:43 +0900

    [21/01/28] solve 2252 - 줄 세우기 including dfs solution

--- by-kotlin/kt-boj/solutions/2252-줄 세우기.kt
+++ by-kotlin/kt-boj/solutions/2252-줄 세우기.kt
@@ -1,32 +1,68 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
-import kotlin.system.exitProcess
 
 private lateinit var adj: Array<MutableList<Int>>
 private lateinit var degrees: Array<Int>
+private lateinit var tour: Array<Boolean>
 private lateinit var resLine: Array<Int>
+private lateinit var stack: LinkedList<Int>
 private var n = 0
 
-fun topologicalSort2252(degrees: Array<Int>) {
-    val q = mutableListOf<Int>()
 
-    for (i in 1..n)
-        if (degrees[i] == 0) q.add(i)
+// 큐와 indegree의 수를 이용한 방법
+// 학습 참고(1): https://m.blog.naver.com/ndb796/221236874984 (나동빈님 블로그)
+//fun topologicalSort2252(degrees: Array<Int>) {
+//    val q = mutableListOf<Int>()
+//
+//    for (i in 1..n)
+//        if (degrees[i] == 0) q.add(i)
+//
+//    for (i in 1..n) {
+//        if (q.isEmpty()) exitProcess(0)
+//
+//        val now = q.removeAt(0)
+//        resLine[i - 1] = now
+//
+//        for (new in adj[now])
+//            if (--degrees[new] == 0) q.add(new)
+//    }
+//
+//    for (x in resLine) {
+//        print("${x} ")
+//    }
+//}
+//
+//fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+//    val nm = readLine().split(" ").map { it.toInt() }.toList()
+//    n = nm[0]
+//
+//    adj = Array(n + 1) { mutableListOf() }
+//    degrees = Array(n + 1) { 0 }
+//    resLine = Array(n) { 0 }
+//
+//    var st: StringTokenizer
+//    repeat(nm[1]) {
+//        st = StringTokenizer(readLine())
+//        val a = st.nextToken().toInt()
+//        val b = st.nextToken().toInt()
+//        adj[a].add(b)
+//        degrees[b]++
+//    }
+//
+//    topologicalSort2252(degrees)
+//}
 
-    for (i in 1..n) {
-        if (q.isEmpty()) exitProcess(0)
 
-        val now = q.removeAt(0)
-        resLine[i - 1] = now
+// 스택과 DFS를 이용한 방법
+// 학습 참고(2): https://jason9319.tistory.com/93
+fun dfs2252(x: Int) {
+    tour[x] = true
 
-        for (new in adj[now])
-            if (--degrees[new] == 0) q.add(new)
-    }
-
-    for (x in resLine) {
-        print("${x} ")
-    }
+    for (new in adj[x])
+        if (!tour[new])
+            dfs2252(new)
+    stack.add(x)
 }
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
@@ -34,8 +70,8 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     n = nm[0]
 
     adj = Array(n + 1) { mutableListOf() }
-    degrees = Array(n + 1) { 0 }
-    resLine = Array(n) { 0 }
+    tour = Array(n + 1) { false }
+    stack = LinkedList(mutableListOf())
 
     var st: StringTokenizer
     repeat(nm[1]) {
@@ -43,8 +79,12 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
         val a = st.nextToken().toInt()
         val b = st.nextToken().toInt()
         adj[a].add(b)
-        degrees[b]++
     }
 
-    topologicalSort2252(degrees)
+    for (i in 1..n)
+        if (!tour[i])
+            dfs2252(i)
+
+    while (stack.isNotEmpty())
+        print("${stack.removeLast()} ")
 }
