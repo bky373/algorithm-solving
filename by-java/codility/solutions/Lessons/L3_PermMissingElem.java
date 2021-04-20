657f65cf67ab7626aaa13d7fa7cff721ad7fa251
bky373 <bo373@naver.com>
2021-04-20 14:00:27 +0900

    [21/04/20] improve solution of L3_PermMissingElem by java

--- by-java/codility/solutions/Lessons/L3_PermMissingElem.java
+++ by-java/codility/solutions/Lessons/L3_PermMissingElem.java
@@ -1,36 +1,22 @@
 package Lessons;
 
 // Lesson3: Time Complexity
-// Task: PermMissingElem
-
-import java.util.Arrays;
+// 시간 복잡도 : O(N)
+// 풀이 참고 : https://jobjava00.github.io/algorithm/codility/lesson3/PermMissingElem/
 
 class L3_PermMissingElem {
     public int solution(int[] A) {
-        if (A.length == 0) return 1;
+        long maxNum = A.length + 1;
+        long sum = 0;
+        for (int num : A)
+            sum += num;
 
-        Arrays.sort(A);
-
-        int lastNum = A[A.length - 1];
-        int[] sortedNums = new int[lastNum - 1];
-
-        for (int i = 0; i < lastNum - 1; i++) {
-            sortedNums[i] = i + 1;
-        }
-
-        for (int i = 0; i < sortedNums.length; i++) {
-            if (A[i] != sortedNums[i]) {
-                return sortedNums[i];
-            }
-        }
-
-        return A.length + 1;
+        return (int) (maxNum * (maxNum + 1) / 2 - sum);
     }
 
     public static void main(String[] args) {
         L3_PermMissingElem s = new L3_PermMissingElem();
         int[] A = {1, 2, 3, 5};
-        A = new int[]{2};
         System.out.println(s.solution(A));
     }
 }
