460e15601aa088b8bfd7343a79a7661a97457697
bky373 <bo373@naver.com>
2021-04-20 18:01:47 +0900

    [21/04/20] improve solution of L4_MissingInteger by java

--- by-java/codility/solutions/Lessons/L4_MissingInteger.java
+++ by-java/codility/solutions/Lessons/L4_MissingInteger.java
@@ -1,26 +1,26 @@
 package Lessons;
 
 // Lesson4: Counting Elements
-// Task: MissingInteger
+// 시간 복잡도: O(N)
 // 풀이 참고: https://jobjava00.github.io/algorithm/codility/lesson4/MissingInteger/
 
 class L4_MissingInteger {
     public int solution(int[] A) {
-        boolean[] checked = new boolean[A.length + 1];
+        int length = A.length;
+        boolean[] existed = new boolean[length + 1];
         int answer = 0;
-        int num;
 
-        for (int i = 0; i < A.length; i++) {
-            num = A[i];
-            if (0 < num && num < checked.length) checked[num] = true;
+        for (int num : A)
+            if (num > 0 && num < existed.length)
+                existed[num] = true;
+
+        for (int i = 1; i < existed.length; i++) {
+            if (!existed[i]) {
+                answer = i;
+                break;
+            }
         }
-
-        for (int i = 1; i < checked.length; i++) {
-            if (checked[i]) answer++;
-            else return i;
-        }
-
-        return answer == (checked.length - 1) ? checked.length : 1;
+        return answer == 0 ? existed.length : answer;
     }
 
     public static void main(String[] args) {
