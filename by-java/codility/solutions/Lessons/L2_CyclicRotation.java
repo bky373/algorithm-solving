0617fa7b9ef1b97f1a7a3a8af26e995192f86653
bky373 <bo373@naver.com>
2021-04-20 13:19:05 +0900

    [21/04/20] improve solution of L2_CyclicRotation by java

--- by-java/codility/solutions/Lessons/L2_CyclicRotation.java
+++ by-java/codility/solutions/Lessons/L2_CyclicRotation.java
@@ -7,24 +7,13 @@ import java.util.Arrays;
 
 class L2_CyclicRotation {
     public int[] solution(int[] A, int K) {
-        if (A.length == 0) return A;
+        int length = A.length;
+        int[] rotated = new int[length];
 
-        int k = K % A.length;
-        if (k == 0) return A;
+        for (int i = 0; i < length; i++)
+            rotated[(i + K) % length] = A[i];
 
-        int[] result = partialReverse(A, 0, A.length - 1);
-        result = partialReverse(result, 0, k - 1);
-        result = partialReverse(result, k, A.length - 1);
-        return result;
-    }
-
-    public int[] partialReverse(int[] nums, int start, int end) {
-        for (int i = 0; i < (end - start) / 2 + 1; ++i) {
-            int temp = nums[(start + i)];
-            nums[start + i] = nums[end - i];
-            nums[end - i] = temp;
-        }
-        return nums;
+        return rotated;
     }
 
     public static void main(String[] args) {
