e8beaec4e407d314aa02d8860027c1b7e06b4b56
bky373 <bo373@naver.com>
2021-04-21 19:17:27 +0900

    [21/04/08] solve 2167 - 2차원 배열의 합 by java

---
+++ by-java/boj/solutions/Main2167.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main2167 {
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] matrix = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        int x1, x2, y1, y2;
+        for (int z = 0; z < K; z++) {
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(matrix[x2][y2] - matrix[x2][y1 - 1] - matrix[x1 - 1][y2] + matrix[x1 - 1][y1 - 1]));
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
