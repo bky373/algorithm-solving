1b2daad56b64ba92348c1d60398431da12f836af
bky373 <bo373@naver.com>
2021-04-22 14:11:44 +0900

    [21/04/22] solve L5_MinAvgTwoSlice by java

--- by-java/codility/solutions/Lessons/L5_MinAvgTwoSlice.java
+++ by-java/codility/solutions/Lessons/L5_MinAvgTwoSlice.java
@@ -1,20 +1,30 @@
 package Lessons;
 
 // Lesson5: Prefix Sums
-// Task: L5_MinAvgTwoSlice
+// 시간 복잡도: O(N)
+// 풀이 참고: https://cheolhojung.github.io/posts/algorithm/Codility-MinAvgTwoSlice.html
 
-import java.util.Arrays;
 
 class L5_MinAvgTwoSlice {
     public int solution(int[] A) {
-        int[] S = new int[A.length];
-        S[0] = A[0];
-        for (int i = 1; i < S.length; i++) {
-            S[i] = S[i-1] + A[i];
-        }
-        System.out.println(Arrays.toString(S));
+        float minAvg = (A[0] + A[1]) / 2f;
+        int minIndex = 0;
+        float currentAvg;
 
-        return 0;
+        for (int i = 2; i < A.length; i++) {
+            currentAvg = (A[i - 2] + A[i - 1] + A[i]) / 3f;
+            if (minAvg > currentAvg) {
+                minAvg = currentAvg;
+                minIndex = i - 2;
+            }
+
+            currentAvg = (A[i - 1] + A[i]) / 2f;
+            if (minAvg > currentAvg) {
+                minAvg = currentAvg;
+                minIndex = i - 1;
+            }
+        }
+        return minIndex;
     }
 
     public static void main(String[] args) {
@@ -24,3 +34,14 @@ class L5_MinAvgTwoSlice {
         System.out.println(s.solution(A));
     }
 }
+
+
+// 풀이 설명 출처 : https://cheolhojung.github.io/posts/algorithm/Codility-MinAvgTwoSlice.html
+// a <= b일 때, a와 b의 평균은 a 이상이 된다.
+// (a=b일 때, a와 b의 평균은 a, 즉 두 수가 같은 경우는 a 혹은 b가 된다.)
+// 마찬가지로, (a+b) <= (c+d)일 때, (a,b)와 (c,d)의 평균은 (a+b) 이상이 된다.
+// 결국, 원소가 4개 (a,b,c,d)인 그룹은 (a,b)와 (c,d)를 나누었을 때,
+// 각각의 평균의 작은 값 이상이 된다.
+// - 2개인 그룹이 작은 값이 되므로 4개의 그룹은 고려할 필요가 없어진다.
+// 예외로 원소가 3개인 그룹에서 2개인 그룹과 1개인 그룹의 경우를 확인해야 하지만,
+// 문제에서 주어진 조건에 의하면 그룹의 원소는 최소 2개 이상이므로 2개와 3개인 그룹만 비교한다.
